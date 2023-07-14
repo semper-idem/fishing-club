5f925d62760fa53ff4765a058ad47f3a0f73e2fa
semper-idem <awojcik010@gmail.com>
2023-07-14 19:13:14 +0200

    Stat - Fish Rarity - Skew grades to lower end so it's not broken

--- src/main/java/net/semperidem/fishingclub/client/game/FishGameLogic.java
+++ src/main/java/net/semperidem/fishingclub/client/game/FishGameLogic.java
@@ -269,9 +269,9 @@ public class FishGameLogic {
         LINE_HEALTH, // Additional health point DONE
         CATCH_RATE,//Percentage reduction of time until bite DONE
         PROGRESS_MULTIPLIER,//Self-explanatory DONE
-        FISH_MAX_WEIGHT_MULTIPLIER,//Moves upper barrier of max fish weight (on average heavier fish)
-        FISH_MAX_LENGTH_MULTIPLIER,//Moves upper barrier of max fish length (on average longer fish)
-        FISH_RARITY_BONUS,//Percentage chance to gain fish on higher grade TODO Skew grades to lower end so it's not broken? idk
-        BITE_WINDOW_MULTIPLIER//Extends ticks in which fish can be hooked
+        FISH_MAX_WEIGHT_MULTIPLIER,//Moves upper barrier of max fish weight (on average heavier fish) DONE
+        FISH_MAX_LENGTH_MULTIPLIER,//Moves upper barrier of max fish length (on average longer fish) DONE
+        FISH_RARITY_BONUS,//Percentage chance to gain fish on higher grade DONE
+        BITE_WINDOW_MULTIPLIER//Extends ticks in which fish can be hooked DONE
     }
 }
